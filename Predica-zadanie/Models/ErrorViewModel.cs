97f9f2c212d6068568f247c2306593629b28e1da
piterm21 <piterm21@gmail.com>
2019-10-19 11:58:55 +0200

    init

---
+++ Predica-zadanie/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Predica_zadanie.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
